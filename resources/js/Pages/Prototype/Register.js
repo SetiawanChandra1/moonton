b3eb79b3cd2aee51be1c8358d4081ef6ab7c2cd5
setiawanchandra <setiawan.chandra@flash-coffee.com>
2022-12-29 01:04:13 +0700

    Feature: Sign In & Sign Up

--- resources/js/Pages/Prototype/Register.js
+++ resources/js/Pages/Prototype/Register.js
@@ -1,12 +1,12 @@
-import Input from "@/Components/Input";
 import Label from "@/Components/Label";
+import Input from "@/Components/Input";
 import Button from "@/Components/Button";
 import { Link, Head } from "@inertiajs/inertia-react";
 
-export default function Login() {
+export default function Register() {
     return (
         <>
-            <Head title="Sign In" />
+            <Head title="Sign Up" />
             <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
                 <div className="fixed top-[-50px] hidden lg:block">
                     <img
@@ -20,7 +20,7 @@ export default function Login() {
                         <img src="/images/moonton-white.svg" alt="" />
                         <div className="my-[70px]">
                             <div className="font-semibold text-[26px] mb-3">
-                                Welcome Back
+                                Sign Up
                             </div>
                             <p className="text-base text-[#767676] leading-7">
                                 Explore our new movies and get <br />
@@ -31,13 +31,26 @@ export default function Login() {
                             <div className="flex flex-col gap-6">
                                 <div>
                                     <Label
+                                        forInput="fullName"
+                                        value="Full Name"
+                                    />
+                                    <Input
+                                        type="text"
+                                        name="fullname"
+                                        placeholder="Your fullname..."
+                                        defaultValue="Angga React"
+                                    />
+                                </div>
+                                <div>
+                                    <Label
                                         forInput="email"
-                                        value="Email Address"
+                                        value="Email Adress"
                                     />
                                     <Input
                                         type="email"
                                         name="email"
-                                        placeholder="Email Address"
+                                        placeholder="Your Email Address"
+                                        value="anggaforreact@fb.com"
                                     />
                                 </div>
                                 <div>
@@ -47,24 +60,22 @@ export default function Login() {
                                     />
                                     <Input
                                         type="password"
-                                        className="rounded-2xl bg-form-bg py-[13px] px-7 w-full focus:outline-alerange focus:outline-none"
-                                        placeholder="Password"
+                                        name="password"
+                                        placeholder="Your Password"
+                                        value="eeeeeeeeeeeeeee"
                                     />
                                 </div>
                             </div>
                             <div className="grid space-y-[14px] mt-[30px]">
-                                <Button type="button" variant="primary">
+                                <Button>
                                     <span className="text-base font-semibold">
-                                        Start Watching
+                                        Sign Up
                                     </span>
                                 </Button>
-                                <Link href={route(`prototype.register`)}>
-                                    <Button
-                                        type="button"
-                                        variant="light-outline"
-                                    >
+                                <Link href={route("prototype.login")}>
+                                    <Button variant="light-outline">
                                         <span className="text-base text-white">
-                                            Create New Account
+                                            Sign In to My Account
                                         </span>
                                     </Button>
                                 </Link>
